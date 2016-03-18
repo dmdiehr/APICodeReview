cd7d40b4b19be38f5bfeb42a695a4d1b7160dfc6
David <ddiehr@yahoo.com>
2016-03-18 12:28:09 -0700

    Add main display logic. Untested.

--- js/main-interface.js
+++ js/main-interface.js
@@ -7,10 +7,30 @@ var getRepos = require('./../js/getRepos.js').getRepos;
 $(document).ready(function(){
   $('#button').click(function(event){
     event.prevenDefault;
-    var input = $('#user-input').val();
-    getRepos(input, apiKey).done(function(response){
-      $('#display').text(response);
-      });
-    })
+    var userName = $('#user-input').val();
+    getRepos(userName, apiKey).done(function(response){
+      console.log(userName)
+      $('#username').text(userName);
+      $('.repo').remove();
+      for(var i=0; i<response.length; i++){
+        var repoName = response[i].name;
+        var description = response[i].description;
+        var created = response[i].created_at.slice(0,10);
+        var updated = response[i].updated_at.slice(0,10);
+        var anchor = "https://developer.github.com/v3/repos/#list-user-repositories"
 
-  });
+        $('#display').append(
+
+          "<div class='repo'><h4>"+repoName+"</h4><p><strong>Description:</strong>"+description+"</p><p class='date'><strong>Created:</strong>"+created+"</p><p class='date'><strong>Updated:</strong>"+updated+"</p><a href='"+anchor+"'>See Repo</a></div>"
+        );
+
+      }
+      $('#display').show();
+    }).fail(function(){
+      if(input.length>0){
+        $('.name').text(userName);
+      }
+      $('#display').show();
+    });
+  });//buton.click
+});//document.ready
