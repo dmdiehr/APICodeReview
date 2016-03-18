e3a0f2f50519907c5c586f4b3e3b6df006e029e7
David <ddiehr@yahoo.com>
2016-03-18 13:24:32 -0700

    touch up interface functionality. works.

--- js/main-interface.js
+++ js/main-interface.js
@@ -1,35 +1,35 @@
 var apiKey = require('./../.env').apiKey;
 var getRepos = require('./../js/getRepos.js').getRepos;
 
-// console.log(apiKey);
-// console.log(getRepos);
-
 $(document).ready(function(){
   $('#button').click(function(event){
     event.prevenDefault;
     var userName = $('#user-input').val();
     getRepos(userName, apiKey).done(function(response){
-      console.log(userName)
-      $('#username').text(userName);
-      $('.repo').remove();
+      console.log(JSON.stringify(response));
+      $('.name').text(userName);
+      $('.blank').hide();
+      $('#example').hide();
       for(var i=0; i<response.length; i++){
         var repoName = response[i].name;
         var description = response[i].description;
+        if (description.length===0){description="None";}
         var created = response[i].created_at.slice(0,10);
         var updated = response[i].updated_at.slice(0,10);
-        var anchor = "https://developer.github.com/v3/repos/#list-user-repositories"
+        var anchor =response[i].html_url;
 
         $('#display').append(
 
-          "<div class='repo'><h4>"+repoName+"</h4><p><strong>Description:</strong>"+description+"</p><p class='date'><strong>Created:</strong>"+created+"</p><p class='date'><strong>Updated:</strong>"+updated+"</p><a href='"+anchor+"'>See Repo</a></div>"
+          "<div class='repo'><h4>"+repoName+"</h4><p><strong>Description: </strong>"+description+"</p><p class='date'><strong>Created: </strong>"+created+"</p><p class='date'><strong>Updated: </strong>"+updated+"</p><a href='"+anchor+"' target='_blank'>See Repo</a></div>"
         );
-
       }
       $('#display').show();
     }).fail(function(){
-      if(input.length>0){
+      if(userName.length>0){
         $('.name').text(userName);
       }
+      $('.blank').show();
+      $('#example').show();
       $('#display').show();
     });
   });//buton.click
